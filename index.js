7d17f435419fca4f061b686185e43fd56a40b37b
Joao Vitor <joaovsantos.dev@gmail.com>
2025-03-18 13:30:14 -0300

    adicionando funcionalidades finais à Pokedex

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const pokemonsContainer = document.querySelectorAll(".container-pokemon");
 const modal = document.querySelector("dialog");
+const inputPesquisa = document.getElementById("pesquisa");
+const btnPesquisa = document.querySelector(".section-pesquisa button");
+console.log(btnPesquisa);
+
+btnPesquisa.addEventListener("click", pesquisaModal);
 
 function mostraPokemon() {
   pokemonsContainer.forEach((item) => {
@@ -7,91 +12,99 @@ function mostraPokemon() {
     const imgPokemon = item.querySelector("img");
     const idPokemon = item.querySelector(".id-pokemon");
     const nomePokemon = item.querySelector(".nome-pokemon");
+    const typePokemon = item.querySelector(".type-pokemon");
 
     fetch(`https://pokeapi.co/api/v2/pokemon/${numeroAleatorio}`)
       .then((r) => r.json())
       .then((b) => {
         imgPokemon.src = b.sprites.front_default;
         idPokemon.innerText = "#" + b.id;
-        nomePokemon.innerText = b.name;
-        mudaCorPokemon(b.types[0].type.name, imgPokemon);
+        nomePokemon.innerText =
+          "Name: " + b.name[0].toUpperCase() + b.name.substring(1);
+        typePokemon.innerText =
+          "Type: " +
+          b.types[0].type.name[0].toUpperCase() +
+          b.types[0].type.name.substring(1);
+
+        mudaCorPokemon(b.types[0].type.name, idPokemon);
+        console.log(b);
       });
   });
 }
 mostraPokemon();
 
-function mudaCorPokemon(tipo, img) {
+function mudaCorPokemon(tipo, id) {
   switch (tipo) {
     case "bug":
-      img.style.backgroundColor = "#699836";
+      id.style.backgroundColor = "#699836";
       break;
 
     case "dark":
-      img.style.backgroundColor = "#86538D";
+      id.style.backgroundColor = "#86538D";
       break;
 
     case "dragon":
-      img.style.backgroundColor = "#50A495";
+      id.style.backgroundColor = "#50A495";
       break;
 
     case "electric":
-      img.style.backgroundColor = "#BFA545";
+      id.style.backgroundColor = "#BFA545";
       break;
 
     case "fairy":
-      img.style.backgroundColor = "#DBB0D5";
+      id.style.backgroundColor = "#DBB0D5";
       break;
 
     case "fighting":
-      img.style.backgroundColor = "#C77039";
+      id.style.backgroundColor = "#C77039";
       break;
 
     case "fire":
-      img.style.backgroundColor = "#BF403B";
+      id.style.backgroundColor = "#BF403B";
       break;
 
     case "flying":
-      img.style.backgroundColor = "#4A65AE";
+      id.style.backgroundColor = "#4A65AE";
       break;
 
     case "ghost":
-      img.style.backgroundColor = "#9D70B1";
+      id.style.backgroundColor = "#9D70B1";
       break;
 
     case "grass":
-      img.style.backgroundColor = "#53A93A";
+      id.style.backgroundColor = "#53A93A";
       break;
 
     case "ground":
-      img.style.backgroundColor = "#957945";
+      id.style.backgroundColor = "#957945";
       break;
 
     case "ice":
-      img.style.backgroundColor = "#72ACC1";
+      id.style.backgroundColor = "#72ACC1";
       break;
 
     case "normal":
-      img.style.backgroundColor = "#A4A4A4";
+      id.style.backgroundColor = "#A4A4A4";
       break;
 
     case "poison":
-      img.style.backgroundColor = "#7D58A2";
+      id.style.backgroundColor = "#7D58A2";
       break;
 
     case "psychic":
-      img.style.backgroundColor = "#BE5C98";
+      id.style.backgroundColor = "#BE5C98";
       break;
 
     case "rock":
-      img.style.backgroundColor = "#5B7B79";
+      id.style.backgroundColor = "#5B7B79";
       break;
 
     case "steel":
-      img.style.backgroundColor = "#5B8196";
+      id.style.backgroundColor = "#5B8196";
       break;
 
     case "water":
-      img.style.backgroundColor = "#559AD2";
+      id.style.backgroundColor = "#559AD2";
       break;
   }
 }
@@ -129,7 +142,6 @@ function mostraModal() {
     fechaModal();
   });
 }
-
 setTimeout(mostraModal, 500);
 
 function fechaModal() {
@@ -137,6 +149,13 @@ function fechaModal() {
 
   botaoFechar.forEach((botao) => {
     botao.addEventListener("click", () => {
+      const selectEvo1 = document.querySelector(".evo1");
+      const selectEvo2 = document.querySelector(".evo2");
+      const selectBase = document.querySelector(".evoBase");
+
+      if (selectEvo1) selectEvo1.remove();
+      if (selectEvo2) selectEvo2.remove();
+      if (selectBase) selectBase.remove();
       modal.close();
     });
   });
@@ -153,47 +172,127 @@ async function mostraEvolucao(nomePoke) {
   const chainEvolucao = await fetch(urlChainEvolucao);
   const chainJson = await chainEvolucao.json();
   const pokeBase = chainJson.chain.species.name;
-  const pokeEvo1 = chainJson.chain.evolves_to[0].species.name;
-  const pokeEvo2 = chainJson.chain.evolves_to[0].evolves_to[0].species.name;
-  if (pokeBase === nomePoke) {
-    const newEvo = document.createElement("img");
 
+  // verifica se o pokemon passado no parâmetro é base
+  if (nomePoke === pokeBase) {
+    const pokeEvo1 = chainJson.chain.evolves_to[0].species.name;
     if (pokeEvo1) {
+      const newEvo = document.createElement("img");
       const selectEvo1 = sectionEvolucao.querySelector(".evo1");
       fetch(`https://pokeapi.co/api/v2/pokemon/${pokeEvo1}`)
         .then((r) => r.json())
         .then((b) => {
-          console.log(b);
-          newEvo.src = b.sprites.front_default;
-          newEvo.classList.add("evo1");
+          Evolucao1 = newEvo;
+          Evolucao1.src = b.sprites.front_default;
           if (!selectEvo1) {
-            sectionEvolucao.appendChild(newEvo);
+            sectionEvolucao.appendChild(Evolucao1);
+            Evolucao1.classList.add("evo1");
           }
         });
     } else {
       console.log("evolucao 1 nao existe");
     }
 
-    if (pokeEvo2) {
+    if (chainJson.chain.evolves_to[0].evolves_to[0]) {
+      const pokeEvo2 = chainJson.chain.evolves_to[0].evolves_to[0].species.name;
+      const newEvo = document.createElement("img");
       const selectEvo2 = sectionEvolucao.querySelector(".evo2");
       fetch(`https://pokeapi.co/api/v2/pokemon/${pokeEvo2}`)
         .then((r) => r.json())
         .then((b) => {
-          console.log(b);
-          newEvo.src = b.sprites.front_default;
-          newEvo.classList.add("evo2");
+          Evolucao2 = newEvo;
+          Evolucao2.src = b.sprites.front_default;
           if (!selectEvo2) {
-            sectionEvolucao.appendChild(newEvo);
+            sectionEvolucao.appendChild(Evolucao2);
+            Evolucao2.classList.add("evo2");
           }
         });
     } else {
       console.log("Evolucao 2 nao existe");
     }
+  } else if (nomePoke == chainJson.chain.evolves_to[0].species.name) {
+    // verifica se o pokemon no parâmetro é a evolucao 1
+    const newEvo = document.createElement("img");
+    const selectBase = sectionEvolucao.querySelector(".evoBase");
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeBase}`)
+      .then((r) => r.json())
+      .then((b) => {
+        evoBase = newEvo;
+        evoBase.src = b.sprites.front_default;
+        if (!selectBase) {
+          sectionEvolucao.appendChild(evoBase);
+          evoBase.classList.add("evoBase");
+        }
+      });
+
+    if (chainJson.chain.evolves_to[0].evolves_to[0]) {
+      const evo2 = chainJson.chain.evolves_to[0].evolves_to[0].species.name;
+      const newEvo = document.createElement("img");
+      const selectEvo2 = sectionEvolucao.querySelector(".evo2");
+      fetch(`https://pokeapi.co/api/v2/pokemon/${evo2}`)
+        .then((r) => r.json())
+        .then((b) => {
+          Evolucao2 = newEvo;
+          Evolucao2.src = b.sprites.front_default;
+          if (!selectEvo2) {
+            sectionEvolucao.appendChild(Evolucao2);
+            Evolucao2.classList.add("evo2");
+          }
+        });
+    }
+  } else {
+    const newEvoBase = document.createElement("img");
+    const newEvo1 = document.createElement("img");
+    const selectBase = sectionEvolucao.querySelector(".evoBase");
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeBase}`)
+      .then((r) => r.json())
+      .then((b) => {
+        evoBase = newEvoBase;
+        evoBase.src = b.sprites.front_default;
+        if (!selectBase) {
+          evoBase.classList.add("evoBase");
+          sectionEvolucao.appendChild(evoBase);
+        }
+      });
+    const pokeEvo1 = chainJson.chain.evolves_to[0].species.name;
+    const selectEvo1 = document.querySelector(".evo1");
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeEvo1}`)
+      .then((r) => r.json())
+      .then((b) => {
+        Evolucao1 = newEvo1;
+        Evolucao1.src = b.sprites.front_default;
+        if (!selectEvo1) {
+          sectionEvolucao.appendChild(Evolucao1);
+          Evolucao1.classList.add("evo1");
+        }
+      });
   }
 }
 
-// fetch(`https://pokeapi.co/api/v2/pokemon/${nomePokemon1}`)
-// .then(r => r.json())
-// .then(b => {
-//   console.log(b)
-// }) // fetch para usar na busca de pokemons
+function pesquisaModal() {
+  const nomePokemon = inputPesquisa.value.toLowerCase();
+  const idPokemonModal = modal.querySelector(".id-pokemon");
+  const nomePokemonModal = modal.querySelector(".nome-pokemon");
+  const imgPrincipal = modal.querySelector(".img-principal");
+  const alturaPokemon = modal.querySelector(".altura-pokemon");
+  const pesoPokemon = modal.querySelector(".peso-pokemon");
+  const habilidade = modal.querySelector(".habilidade-pokemon");
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${nomePokemon}`)
+    .then((r) => r.json())
+    .then((b) => {
+      idPokemonModal.innerText = "#" + b.id;
+      nomePokemonModal.innerText = b.name;
+      imgPrincipal.src = b.sprites.front_default;
+      mudaCorPokemon(b.types[0].type.name, imgPrincipal);
+
+      if (b.height / 10 >= 1) alturaPokemon.innerText = b.height / 10 + "m";
+      else alturaPokemon.innerText = b.height / 10 + "cm";
+
+      pesoPokemon.innerText = b.weight / 10 + "kg";
+      habilidade.innerText = b.abilities[0].ability.name;
+      mostraEvolucao(nomePokemonModal.innerText);
+      modal.showModal();
+    });
+  fechaModal();
+}
